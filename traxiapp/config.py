978fccbdb5df52648e9b405342cbcabbc34dd35f
traxi <traxibv@gmail.com>
2018-11-09 05:05:23 +0100

    Nog meer geklooi.

--- traxiapp/config.py
+++ traxiapp/config.py
@@ -4,12 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG=False
-    SECRET_KEY=os.environ.get('TRAXI_DB_PASS')
+    SECRET_KEY = os.environ.get('TRAXI_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 
 # Development server
-class DevConfig(Config):
+class DevelopmentConfig(Config):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='postgresql:///traxi'
-
