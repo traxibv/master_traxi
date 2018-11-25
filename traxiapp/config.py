7ad957501eed5bdfc3b7f037070a70884fb9c12e
traxibv <traxib@gmail.com>
2018-11-25 18:50:07 +0100

    Changed config to work on dev server

--- traxiapp/config.py
+++ traxiapp/config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG=False
-    SECRET_KEY = os.environ.get('TRAXI_KEY') or 'super secret'
+    SECRET_KEY = os.environ.get('TRAXI_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 
 # Development server
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost/traxi'
+    SQLALCHEMY_DATABASE_URI='postgresql:///traxi'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
     SECURITY_PASSWORD_SALT = 'fhasdgihwntlgy8f'
 
