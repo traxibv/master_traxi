3822098758e5deb920d2e13614e38674c55e1859
vanozi <wouter.vanos86@gmail.com>
2018-11-30 12:08:23 +0100

    added Role model and UserRoles model to models.py

--- traxiapp/models.py
+++ traxiapp/models.py
@@ -8,8 +8,19 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    roles = db.relationship('Role', secondary='user_roles',backref=db.backref('users', lazy='dynamic'))
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+class Role(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+
+class UserRoles(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
