4dd60f16e38ac77720a149b1722ff7d9dc7e6e3d
Wouter van Os <wouter.vanos86@gmail.com>
2019-02-19 22:05:02 +0100

    Added configuration to provide: - upload destiation path for profile pictures - url path for profile pictures

--- traxiapp/config.py
+++ traxiapp/config.py
@@ -12,3 +12,5 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql:///traxi'
+    UPLOADED_PROFILEPICTURES_DEST = basedir + '/traxiapp/static/profile_pics/'
+    UPLOADED_PROFILEPICTURES_URL = 'http://traxiworld.com/static/profile_pics/'
