c45a20a380ede5ecc316ffe023f8c2834335fa53
Wouter van Os <wouter.vanos86@gmail.com>
2019-02-27 23:02:38 +0100

    adjusted the upload button

--- traxiapp/config.py
+++ traxiapp/config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
-    SECRET_KEY = os.environ.get('TRAXI_KEY')
+    SECRET_KEY = os.environ.get('TRAXI_KEY') or 'supergeheim'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # Development server
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///traxi'
-    UPLOADED_PROFILEPICTURES_DEST = basedir + '/traxiapp/static/profile_pics/'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/traxi'
+    UPLOADED_PROFILEPICTURES_DEST = basedir + '/static/profile_pics/'
     UPLOADED_PROFILEPICTURES_URL = 'http://traxiworld.com/static/profile_pics/'
